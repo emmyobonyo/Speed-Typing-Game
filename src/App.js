c777072454d8b9ed6c641334e8a1229dc93215d7
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-04-12 14:25:45 +0300

    Track text-area state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+function App() {  
+  const [text, setText] = useState("")
+  
+  const handleChange = (e) => {
+      const {value} = e.target
+      setText(value)
+  }
+  
   return (
     <div>
         <h1>How fast do you type?</h1>
-        <textarea />
-        <h4>Time reminaing: ???</h4>
+        <textarea
+            onChange={handleChange}
+            value={text}
+        />
+        <h4>Time remaining: ???</h4>
         <button>Start</button>
         <h1>Word count: ???</h1>
     </div>
-  );
+  )
 }
 
 export default App;
